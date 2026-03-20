3ca023567b4a53b13f49eb1e8367f8593868c10d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-20 12:12:15 -0600

    feat: metodo para obtener el historial

--- src/api/services/CartsService.ts
+++ src/api/services/CartsService.ts
@@ -1,9 +1,14 @@
 import { API_CONFIG } from "../../config/api";
-import type { Entry } from "../../types/types";
+import type { Entry, History } from "../../types/types";
 import { apiClient } from "../client";
 
 class CartsService {
+  private getHistoryEndpoint = API_CONFIG.endpoints.Carts.getHistory;
   private entryEndpoint = API_CONFIG.endpoints.Carts.entry;
+
+  async getHistory(): Promise<History[]> {
+    return apiClient.get<History[]>(this.getHistoryEndpoint);
+  }
 
   async entry(data: Entry): Promise<void> {
     return apiClient.post<void>(this.entryEndpoint, data);
