f8f696d9d4c5190d0aa338a54806a86de60efd56
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-01 11:13:29 -0600

    feat: metodo para obtener los meses disponibles

--- src/api/services/CartsService.ts
+++ src/api/services/CartsService.ts
@@ -1,15 +1,21 @@
 import { API_CONFIG } from "../../config/api";
-import type { Entry, History } from "../../types/types";
+import type { AvailableMonth, Entry, History } from "../../types/types";
 import { apiClient } from "../client";
 
 class CartsService {
   private getHistoryEndpoint = API_CONFIG.endpoints.Carts.getHistory;
+  private getAvailableMonthsEndpoint =
+    API_CONFIG.endpoints.Carts.availabelMonths;
   private reportEndpoint = API_CONFIG.endpoints.Carts.report;
   private entryEndpoint = API_CONFIG.endpoints.Carts.entry;
   private exitEndpoint = API_CONFIG.endpoints.Carts.exit;
 
   async getHistory(): Promise<History[]> {
     return apiClient.get<History[]>(this.getHistoryEndpoint);
+  }
+
+  async getAvailableMonths(): Promise<AvailableMonth[]> {
+    return apiClient.get<AvailableMonth[]>(this.getAvailableMonthsEndpoint);
   }
 
   async getReport(year: number, month: number): Promise<void> {
