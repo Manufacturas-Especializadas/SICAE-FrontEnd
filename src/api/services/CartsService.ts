5c3b2ede2d4a54f13b3b38df1e49bc61a4a40793
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-01 11:03:19 -0600

    feat: metodo para generar el reporte mensual

--- src/api/services/CartsService.ts
+++ src/api/services/CartsService.ts
@@ -4,11 +4,19 @@ import { apiClient } from "../client";
 
 class CartsService {
   private getHistoryEndpoint = API_CONFIG.endpoints.Carts.getHistory;
+  private reportEndpoint = API_CONFIG.endpoints.Carts.report;
   private entryEndpoint = API_CONFIG.endpoints.Carts.entry;
   private exitEndpoint = API_CONFIG.endpoints.Carts.exit;
 
   async getHistory(): Promise<History[]> {
     return apiClient.get<History[]>(this.getHistoryEndpoint);
+  }
+
+  async getReport(year: number, month: number): Promise<void> {
+    const url = `${this.reportEndpoint}${year}/${month}`;
+    const fileName = `Reporte_Carritos_${month}_${year}.xlsx`;
+
+    return apiClient.downloadFile(url, fileName);
   }
 
   async entry(data: Entry): Promise<void> {
