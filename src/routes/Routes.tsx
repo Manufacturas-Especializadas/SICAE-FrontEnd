2fa3092cbe0483a3f2c9f75d96fd20954b993581
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-01 11:14:40 -0600

    feat: ruta de reportes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { CartDashboard } from "../pages/CartDashboard/CartDashboard";
+import { ReportsIndex } from "../pages/Reports/ReportsIndex";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<CartDashboard />} />
+      <Route path="/reportes" element={<ReportsIndex />} />
     </Routes>
   );
 };
